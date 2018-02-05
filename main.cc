a66e5003b721ab1e587e6c68a1499507028ed1cc
trree <wufl@koal.com>
2018-02-05 19:05:28 +0800

    update limit function

--- main.cc
+++ main.cc
@@ -6,16 +6,19 @@ int main()
 {
   indefinite::IndefiniteStream<int> a(3);
   a.filter([](int value) {return (value % 5 == 0);}).filter([](int value) {return (value % 9 == 0);});
-  
+
+  std::cout << "One by one: " << '\n';
   std::cout << a.top() << '\n';
   std::cout << a.next() << '\n';
   std::cout << a.next() << '\n';
   std::cout << a.next() << '\n';
   std::cout << a.next() << '\n';
+  std::cout << '\n';
   
-  std::vector<int> v = a.limit(10);
+  int size = 10;
+  std::cout << "Get " << size << " numbers: " << '\n';
+  std::vector<int> v = a.limit(size);
   std::for_each(v.begin(), v.end(), [](int value){ std::cout << value << ' '; });
-  a.printstream();
 
   return 0;
 }
